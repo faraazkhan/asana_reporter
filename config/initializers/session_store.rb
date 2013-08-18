450835381cc72e66f0baa7740888ded7c0c2bf61
Faraaz Khan <faraaz@rationalizeit.us>
2013-08-18 18:26:56 -0400

    removing references to bdd helper

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AsanaReporter::Application.config.session_store :cookie_store, key: '_bdd_helper_session'
+AsanaReporter::Application.config.session_store :cookie_store, key: '_asana_reporter_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
