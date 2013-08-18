f26eea5596aeffea1fb88475c2ae431cf451bfb7
Faraaz Khan <faraaz@rationalizeit.us>
2013-08-18 18:36:13 -0400

    adding host for mail delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ AsanaReporter::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { host: 'bddatcgi.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'asanareporter.herokuapp.com' }
   config.force_ssl = true
 end
